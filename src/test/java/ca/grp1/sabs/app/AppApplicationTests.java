28f5e26187f082bbfcead148fa02d94224d0dd82
Koder <140269220+rsmith319@users.noreply.github.com>
2026-03-18 20:22:05 -0400

    update

---
+++ src/test/java/ca/grp1/sabs/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.grp1.sabs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
