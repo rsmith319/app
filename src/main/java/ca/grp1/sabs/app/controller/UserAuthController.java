4cbf339c635169e786fd547d1d12c93e1ee4f1a3
Koder <140269220+rsmith319@users.noreply.github.com>
2026-03-27 19:56:32 -0400

    Update UserAuthController.java

--- src/main/java/ca/grp1/sabs/app/controller/UserAuthController.java
+++ src/main/java/ca/grp1/sabs/app/controller/UserAuthController.java
@@ -4,13 +4,22 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import ca.grp1.sabs.app.model.UserSchema;
 import ca.grp1.sabs.app.repo.UserRepository;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 public class UserAuthController {
 
     private final UserRepository userRepository;
